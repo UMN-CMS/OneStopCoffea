59fe2c17743fd220bf59f11da6925bcf13ee83e8
Charlie Kapsiak <kapsi002@umn.edu>
2026-06-16 16:43:27 -0500

    Update cutflows

--- analyzer/postprocessing/cutflows.py
+++ analyzer/postprocessing/cutflows.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 import functools as ft
 from typing import Literal
 from .style import StyleSet
@@ -47,6 +48,9 @@ class PlotSelectionFlow(BasePostprocessor):
 class CutflowTable(BasePostprocessor):
     output_name: str
     format: Literal["markdown", "csv", "latex"] = "csv"
+    key: str = "{dataset_name}"
+    standalone: bool = False
+    highlight_rows: list[tuple[int,str]] | None  = None
 
     def getRunFuncs(self, group, prefix=None):
         common_meta = commonDict(group)
@@ -60,16 +64,20 @@ class CutflowTable(BasePostprocessor):
             common_meta,
             output_path,
             format=self.format,
+            key=self.key,
+            standalone=self.standalone,
+            highlight_rows=self.highlight_rows
         )
 
 
-def makeCutflowDf(group):
+def makeCutflowDf(group, key="{dataset_name}"):
     import pandas as pd
 
     dataset_cutflows = {}
     cut_order = None
     for selection_flow, metadata in group:
-        dataset_cutflows[metadata["dataset_name"]] = _getCutflow(selection_flow)
+        k = dotFormat(key, **dict(dictToDot(metadata)))
+        dataset_cutflows[k] = _getCutflow(selection_flow)
         if cut_order is None:
             cut_order = list(selection_flow.cuts)
         else:
@@ -91,11 +99,61 @@ def makeCutflowDf(group):
     return df
 
 
-def makeAndSaveCutflowTable(group, common_meta, output_path, format="csv"):
-    df = makeCutflowDf(group)
+STANDALONE_TOP = r"""\documentclass{standalone}
+\usepackage{booktabs}
+\usepackage[table,usenames,svgnames]{xcolor}
+\begin{document}
+"""
+
+STANDALONE_BOTTOM = r"""
+\end{document}
+"""
+
+
+def makeAndSaveCutflowTable(
+    group,
+    common_meta,
+    output_path,
+    format="csv",
+    key="{dataset_name}",
+    standalone=False,
+    highlight_rows=None,
+):
+    import numpy as np
+
+    highlight_rows = highlight_rows or []
+
+    df = makeCutflowDf(group, key=key)
+    output_path = Path(output_path)
+    output_path.parent.mkdir(exist_ok=True, parents=True)
+
+    s = (
+        df.style.apply(
+            lambda x: np.where(
+                (np.arange(len(x)) % 6 > 2), "background-color: lightgray", ""
+            ),
+            axis=1,
+        )
+        .format("{:0.2f}", escape="latex")
+        .format_index(escape="latex", axis=0)
+        # .format_index(escape="latex",axis=1)
+    )
+    for row,color in highlight_rows:
+        s = s.apply(
+            lambda x: np.where(
+                (np.arange(len(x)) == row), f"background-color: {color}", ""
+            ),
+            axis=0,
+        )
+
     if format == "csv":
         df.to_csv(output_path)
     elif format == "markdown":
-        df.to_markdown(output_path)
+        s.to_markdown(output_path, convert_css=True, **kwargs)
     elif format == "latex":
-        df.to_latex(output_path)
+        text = s.to_latex(None, convert_css=True)
+        if standalone:
+            text = STANDALONE_TOP + text + STANDALONE_BOTTOM
+
+        with open(output_path, "w") as f:
+            f.write(text)
