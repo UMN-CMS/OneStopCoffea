4ceb111264f4aac2a505dfd81a97d270375fca3d
Madison Sherouse <shero031@umn.edu>
2026-07-08 14:25:32 -0500

    work from 7/8/2026 in an effort to produce 2D efficiency plot

---
+++ analyzer/postprocessing/aggregate_plot_Eff2D_temporary.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import functools as ft
+from typing import Literal
+import itertools as it
+from .style import Style
+from analyzer.utils.structure_tools import (
+    commonDict,
+    dictToDot,
+    dotFormat,
+)
+from .processors import BasePostprocessor
+from analyzer.utils.querying import deepLookup
+from attrs import define, field
+import numpy as np
+import enum
+
+def efficiency(passing_events, all_events):
+    return passing_events/all_events
+
+def makeEfficiency2D(
+    total_group,
+    passing_group,
+    common_metadata,
+    output_path,
+    xy_pattern,
+    xyz_labels,
+    style,
+    plot_configuration=None,
+    **kwargs,
+):
+    from .plots.annotations import addCMSBits
+    from .plots.common import PlotConfiguration
+    from .plots.utils import saveFig
+
+    passing_lookup = {}
+
+    for item, meta in passing_group:
+        pass_h = item.histogram
+        xy = (
+            float(deepLookup(meta, xy_pattern[0])),
+            float(deepLookup(meta, xy_pattern[1])),
+        )
+        passing_lookup[xy] = item.histogram
+
+    effs = []
+    for item, meta in total_group:
+        xy = (
+            float(deepLookup(meta, xy_pattern[0])),
+            float(deepLookup(meta, xy_pattern[1])),
+        )
+        total_h = item.histogram
+
+        eff = (efficiency(pass_h.values(), total_h.values())
+        )
+        effs.append((*xy, eff))
+    
+    effs = np.array(effs)
+
+    fig, ax = plt.subplots()
+    sc = ax.scatter(
+        effs[:, 0],
+        effs[:, 1],
+        c=effs[:, 2],
+        **style.get("scatter_z", include_type=False),
+    )
+    fig.colorbar(sc, ax=ax, label=xyz_labels[2])
+
+    pc = plot_configuration or PlotConfiguration()
+    addCMSBits(
+        ax,
+        [x.metadata for x in total_group],
+        plot_configuration=pc,
+    )
+    ax.set_xlabel(xyz_labels[0])
+    ax.set_ylabel(xyz_labels[1])
+
+    saveFig(fig, output_path, extension=pc.image_type)
+    plt.close(fig)
+
+@define
+class Efficiency2D(BasePostprocessor):
+    output_name: str
+    group_xy_patterns: tuple[list[str], list[str]]
+    xyz_labels: tuple[str, str, str]
+    style: Style = field(factory=Style)
+
+    def getRunFuncs(self, group, prefix=None):    # The stuff I want needs to be in a group
+        total = group["total"]
+        passing = group["passing"]
+        common_meta = commonDict(it.chain(total, passing))
+        output_path = dotFormat(
+            self.output_name, **dict(dictToDot(common_meta)), prefix=prefix
+        )
+        pc = self.plot_configuration.makeFormatted(common_meta)
+
+        yield ft.partial(
+            makeEfficiency2D,
+            total_group=total,
+            passing_group=passing,
+            output_path=output_path,
+            xy_pattern=self.group_xy_patterns,
+            xyz_labels=self.xyz_labels,
+            plot_configuration=pc,
+            style=self.style,
+        )
--- analyzer/postprocessing/aggregate_plots.py
+++ analyzer/postprocessing/aggregate_plots.py
@@ -113,72 +113,6 @@ def makeSignificance2D(
     saveFig(fig, output_path, extension=pc.image_type)
     plt.close(fig)
 
-def efficiency(passing_events, all_events):
-    return passing_events/all_events
-
-def makeEfficiency2D(
-    total_group,
-    passing_group,
-    output_path,
-    xy_pattern,
-    xyz_labels,
-    style,
-    plot_configuration=None,
-    **kwargs,
-):
-    from .plots.annotations import addCMSBits
-    from .plots.common import PlotConfiguration
-    from .plots.utils import saveFig
-
-    passing_lookup = {}
-
-    
-
-    for item, meta in passing_group:
-        pass_h = item.histogram
-        xy = (
-            float(deepLookup(meta, xy_pattern[0])),
-            float(deepLookup(meta, xy_pattern[1])),
-        )
-        passing_lookup[xy] = item.histogram
-
-    effs = []
-    for item, meta in total_group:
-        xy = (
-            float(deepLookup(meta, xy_pattern[0])),
-            float(deepLookup(meta, xy_pattern[1])),
-        )
-        total_h = item.histogram
-
-        eff = (efficiency(pass_h.values(), total_h.values())
-        )
-        effs.append((*xy, eff)) 
-        if pass_h is None:
-            continue
-
-    effs = np.array(effs)
-
-    fig, ax = plt.subplots()
-    sc = ax.scatter(
-        effs[:, 0],
-        effs[:, 1],
-        c=effs[:, 2],
-        **style.get("scatter_z", include_type=False),
-    )
-    fig.colorbar(sc, ax=ax, label=xyz_labels[2])
-
-    pc = plot_configuration or PlotConfiguration()
-    addCMSBits(
-        ax,
-        [x.metadata for x in total_group],
-        plot_configuration=pc,
-    )
-    ax.set_xlabel(xyz_labels[0])
-    ax.set_ylabel(xyz_labels[1])
-
-    saveFig(fig, output_path, extension=pc.image_type)
-    plt.close(fig)
-
 @define
 class Significance2D(BasePostprocessor):
     output_name: str
@@ -214,29 +148,3 @@ class Significance2D(BasePostprocessor):
             style=self.style,
         )
 
-@define
-class Efficiency2D(BasePostprocessor):
-    output_name: str
-    group_xy_patterns: tuple[list[str], list[str]] 
-    xyz_labels: tuple[str, str, str]
-    style: Style = field(factory=Style)
-
-    def getRunFuncs(self, group, prefix=None):    # The stuff I want needs to be in a group
-        total = group["total"]
-        passing = group["passing"]
-        common_meta = commonDict(it.chain(total, passing))
-        output_path = dotFormat(
-            self.output_name, **dict(dictToDot(common_meta)), prefix=prefix
-        )
-        pc = self.plot_configuration.makeFormatted(common_meta)
-
-        yield ft.partial(
-            makeEfficiency2D,
-            total_group=total,
-            passing_group=passing,
-            output_path=output_path,
-            xy_pattern=self.group_xy_patterns,
-            xyz_labels=self.xyz_labels,
-            plot_configuration=pc,
-            style=self.style,
-        )
