feb57ff2ceb6fc5629d7594998f166ab3e123d3f
Madison Sherouse <shero031@umn.edu>
2026-07-17 12:46:53 -0500

    Efficiency2D draft, module is working.

--- analyzer/postprocessing/aggregate_plot_Eff2D_temporary.py
+++ analyzer/postprocessing/aggregate_plot_Eff2D_temporary.py
@@ -16,6 +16,9 @@ from analyzer.utils.querying import deepLookup
 from attrs import define, field
 import numpy as np
 import enum
+import awkward as ak
+import matplotlib.pyplot as plt
+from .grouping import GroupBuilder
 
 def efficiency(passing, all_events):
     return passing/all_events
@@ -36,52 +39,89 @@ def makeEfficiency2D(
     from .plots.utils import saveFig
 
     passing_lookup = {}
-
-    for item, meta in passing_group:
-        pass_h = item.histogram
-
-        xy = (
-         float(deepLookup(meta, xy_pattern[0])),
-         float(deepLookup(meta, xy_pattern[1])),
-        )
-        passing_lookup[xy] = item.histogram
-
     effs = []
+    print(type(passing_group))
+    for item, meta in passing_group:
+        #print(deepLookup(meta, xy_pattern[0]), deepLookup(meta, xy_pattern[1]))
+        xy = (float(deepLookup(meta, xy_pattern[0])), float(deepLookup(meta, xy_pattern[1])))
+        passing_lookup[xy] = item.histogram
+    print(len(passing_group))
     for item, meta in total_group:
-        xy = (
-            float(deepLookup(meta, xy_pattern[0])),
-            float(deepLookup(meta, xy_pattern[1])),
-        )
+        xy = (float(deepLookup(meta, xy_pattern[0])), float(deepLookup(meta, xy_pattern[1])))
         total_h = item.histogram
-
-        eff = (efficiency(pass_h.values(), total_h.values())
-        )
+        pass_h = passing_lookup.get(xy)
+        if pass_h is None:
+            print(f"Warning: no matching passing hists for {xy}, skipping")
+            continue
+        eff = efficiency(pass_h.values().sum(), total_h.values().sum())
         effs.append((*xy, eff))
-    
-    print(effs)
-    effs = np.array(effs)
 
+    #print(xy_pattern[0])
+    #print(len(passing_lookup))
+    effs = ak.Array(effs)
+    xs = effs["0"]
+    ys = effs["1"]
+    eff_values = effs["2"]
+    
+    print("xs:", xs)
+    print("ys:", ys)
+    print("eff_values:", eff_values)
     fig, ax = plt.subplots()
-    sc = ax.scatter(
-        effs[:, 0],
-        effs[:, 1],
-        c=effs[:, 2],
-        **style.get("scatter_z", include_type=False),
+
+    xvals = np.unique(ak.to_numpy(xs))
+    yvals = np.unique(ak.to_numpy(ys))
+
+    xs_np = ak.to_numpy(xs)
+    ys_np = ak.to_numpy(ys)
+    eff_values_np = ak.to_numpy(eff_values)
+
+# Create empty grid
+    Z = np.full((len(yvals), len(xvals)), np.nan)
+
+# Fill existing (x, y) bins
+    for x, y, eff in zip(xs_np, ys_np, eff_values_np):
+        ix = np.where(xvals == x)[0][0]
+        iy = np.where(yvals == y)[0][0]
+        Z[iy, ix] = eff
+
+    # Build bin edges
+    dx = np.diff(xvals).mean()
+    dy = np.diff(yvals).mean()
+
+    xedges = np.concatenate(
+        ([xvals[0] - dx / 2], xvals[:-1] + dx / 2, [xvals[-1] + dx / 2])
     )
-    fig.colorbar(sc, ax=ax, label=xyz_labels[2])
+    yedges = np.concatenate(
+        ([yvals[0] - dy / 2], yvals[:-1] + dy / 2, [yvals[-1] + dy / 2])
+    )
+
+    pcm = ax.pcolormesh(
+        xedges,
+        yedges,
+        Z,
+        cmap="viridis",
+        shading="auto",
+        vmin=0,
+        vmax=1,
+    )
+
+    fig.colorbar(pcm, ax=ax, label="Efficiency")
 
     pc = plot_configuration or PlotConfiguration()
+
     addCMSBits(
         ax,
         [x.metadata for x in total_group],
         plot_configuration=pc,
     )
+
     ax.set_xlabel(xyz_labels[0])
     ax.set_ylabel(xyz_labels[1])
 
     saveFig(fig, output_path, extension=pc.image_type)
-    plt.close(fig)
 
+
+    plt.close(fig)
 @define
 class Efficiency2D(BasePostprocessor):
     output_name: str
@@ -89,19 +129,24 @@ class Efficiency2D(BasePostprocessor):
     xyz_labels: tuple[str, str, str]
     style: Style = field(factory=Style)
 
+
     def getRunFuncs(self, Group, prefix=None):
-        total = Group["name_grouping"][1]["total"]
-        passing = Group["name_grouping"][1]["passing"]
-        common_meta = commonDict(it.chain(total, passing))
+        passing_group = []
+        total_group = []
+        for dataset_group in Group["name_grouping"]:
+            passing_group.extend(dataset_group["passing"])
+            total_group.extend(dataset_group["total"])
+
+        common_meta = commonDict(it.chain(total_group, passing_group))
         output_path = dotFormat(
-            self.output_name, **dict(dictToDot(common_meta)), prefix=prefix
-        )
+        self.output_name, **dict(dictToDot(common_meta)), prefix=prefix
+    )
         pc = self.plot_configuration.makeFormatted(common_meta)
 
         yield ft.partial(
             makeEfficiency2D,
-            total_group=total,
-            passing_group=passing,
+            total_group=total_group,
+            passing_group=passing_group,
             common_metadata=common_meta,
             output_path=output_path,
             xy_pattern=self.group_xy_patterns,
